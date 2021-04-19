13ee9c2a4c8a0d1984ef43db0b9191bebeb1c284
ostimar@gmail.com <ostimar@gmail.com>
2021-04-19 22:15:58 +0200

    Probado

---
+++ src/test/java/com/practica/userJava/UserJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.userJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
